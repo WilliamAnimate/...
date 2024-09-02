9a48fd202cafc848b3df5049bb25bd270fc6c337
WilliamAnimate <nyameowmeow@duck.com>
2024-09-01 20:52:03 -0400

    feat: sway (to my cosmos)
    (no i don't actually play that game)
    (no im not gonna use cosmic)
    (i have a mental illness)

---
+++ .config/fish/config.fish
@@ -0,0 +1,75 @@
+source $HOME/.config/fish/yeet.fish # sway autostart when logging into getty@tty1 (assuming yuor'e on systemd. idk how others work hehe)
+
+# https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+# my fingers speak vi
+function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+end
+
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
+
+# set -g theme_display_vi yes
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_nerd_fonts yes
+set -g theme_display_date no
+set -g theme_color_scheme catppuccin-mocha
+
+export EDITOR=nvim
+
+alias sudo='doas'
+alias v='nvim'
+alias vi='nvim'
+# alias vim='nvim'
+alias mem='~/.config/scripts/memory'
+alias vol='~/.config/scripts/volume'
+alias bright='~/.config/scripts/brightness'
+
+# source: https://github.com/CachyOS/cachyos-fish-config/
+# Format man pages
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+## Useful aliases
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                   # show only dotfiles
+
+# why the fuck less so cringe??? show colors
+# this doesn't work half the time though
+alias less='less -R'
+
+# Common use
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
---
+++ .config/nvim/init.lua
@@ -0,0 +1,67 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+-- rust
+require'lspconfig'.rust_analyzer.setup{}
+-- clang (c++)
+require'lspconfig'.clangd.setup{}
+
+require("base46").toggle_transparency()
+-- vim.opt.listchars = { trail = "·" }
+vim.opt.list = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.api.nvim_win_set_option(0, 'colorcolumn', '110')
+vim.cmd('highlight ColorColumn ctermbg=0 guibg=#0c0c0c')
+vim.opt.showtabline = 0
+vim.g.toggle_theme_icon = nil
+
+vim.api.nvim_exec("aunmenu PopUp.How-to\\ disable\\ mouse", false)
+vim.api.nvim_exec("aunmenu PopUp.-1-", false)
+-- vim.api.nvim_exec("set mouse=", false) -- masochist mode: disables the mouse
+vim.api.nvim_exec("set ttyfast", false)
+vim.api.nvim_exec("set lazyredraw", false)
+vim.api.nvim_exec("noswap", false)
+vim.api.nvim_exec("set noswapfile", false)
+
