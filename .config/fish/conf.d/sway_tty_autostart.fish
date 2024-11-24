eaf94c788cf6ff9d0a741d8d8db99f9598632161
WilliamAnimate <nyameowmeow@duck.com>
2024-11-24 11:53:51 -0500

    feat!: i use artix btw

--- .config/fish/conf.d/sway_tty_autostart.fish
+++ .config/fish/conf.d/sway_tty_autostart.fish
@@ -4,6 +4,6 @@ if [ (tty) = "/dev/tty1" ]
     export QT_QPA_PLATFORM wayland
     export XDG_CURRENT_DESKTOP sway
     export XDG_SESSION_DESKTOP sway
-    exec sway
+    exec dbus-run-session sway
 end
 
